c077c3c484bdafad629480c6c185effb43f816d6
apdonaghy <adonaghy@seattleu.edu>
2020-01-24 17:36:31 -0800

    finished project 2

--- 2-fizzbuzz-fun/index.js
+++ 2-fizzbuzz-fun/index.js
@@ -1,28 +1,36 @@
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: The same output as the FizzBuzz algorithm but as a function.
+Input: integer
+Output: Prints out _fizz_ if the number is divisble by 3, _buzz_ if it is divisible by 5, and _fizzbuzz_ if it is divisible by both 3 and 5.
+Usage: Node 2-fizzbuzz <integer>
 */
 
-// get input using getargs
 const getargs = require("../modules/getargs")
 let input = getargs.getIntegerArg()
 
 
 function fizzbuzz(n){
-// TODO provide for tests for divisible for 3,5, and 15 to return desired output
-}
-
-// TODO check if input is not an integer
-if ( ) {
-  // TODO provide usage statement
-}
-else {
-  // TODO provide info to for loop to iterate from 1 to value of input
-  for ( ) {
-    console.log(`${i} ${fizzbuzz(i)}`);
+  if(n % 15 == 0){
+    return 'fizzbuzz';
+   }
+  else if(n % 3 == 0) {
+    return 'fizz';
+   }
+  else if(n % 5 == 5){
+    return 'buzz';
+  }
+  else {
+    return ``;
   }
 }
 
 
+if (isNaN(input) || !Number.isInteger(input)) {
+  console.log("usage: node 1-fizzbuzz <integer>");
+}
+
+else {
+  for (let i = 1; i<=input; i++) {
+    console.log(`${i} ${fizzbuzz(i)}`);
+  }
+}
